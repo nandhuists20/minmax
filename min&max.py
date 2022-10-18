ceb870bcb282f0fb76c11b37ec036c9523f3554d
nandhuists20 <111509467+nandhuists20@users.noreply.github.com>
2022-10-18 17:48:54 +0530

    Add files via upload

---
+++ min&max.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 17:30:48 2022
+
+@author: nandu
+"""
+
+# Python program to rearrange an array in minimum
+# maximum form
+
+# Prints max at first position, min at second position
+# second max at third position, second min at fourth
+# position and so on.
+
+
+def rearrange(arr, n):
+	# Auxiliary array to hold modified array
+	temp = n*[None]
+
+	# Indexes of smallest and largest elements
+	# from remaining array.
+	small, large = 0, n-1
+
+	# To indicate whether we need to copy remaining
+	# largest or remaining smallest at next position
+	flag = True
+
+	# Store result in temp[]
+	for i in range(n):
+		if flag is True:
+			temp[i] = arr[large]
+			large -= 1
+		else:
+			temp[i] = arr[small]
+			small += 1
+
+		flag = bool(1-flag)
+
+	# Copy temp[] to arr[]
+	for i in range(n):
+		arr[i] = temp[i]
+	return arr
+
+
+# Driver code
+arr = [1, 2, 3, 4, 5, 6]
+n = len(arr)
+print("Original Array")
+print(arr)
+print("Modified Array")
+print(rearrange(arr, n))
+
+# This code is contributed by Pratik Chhajer
